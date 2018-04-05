3eae77893dceeb08e613952d96ef4208acab48e4
gedar <gedar0082@gmail.com>
2018-04-05 13:04:05 +0300

    добавил работу счётчика самой длинной последовательности по переменной. добавил возврат значений по координатам вместо возврата всего массива

--- src/TicTacToeTest.java
+++ src/TicTacToeTest.java
@@ -13,14 +13,14 @@ class TicTacToeTest {
         testField1.addMark(0,0, TicTacToe.Mark.X);
         TicTacToe.Mark[][] result1 = new TicTacToe.Mark[3][3];
         result1[0][0] = TicTacToe.Mark.X;
-        assertArrayEquals(result1,testField1.getField());
+        assertEquals(result1[0][0],testField1.getPoint(0,0));
 
         //тест на добавление нолика
         TicTacToe testField2 = new TicTacToe(3);
-        testField2.addMark(0,0, TicTacToe.Mark.X);
+        testField2.addMark(0,0, TicTacToe.Mark.O);
         TicTacToe.Mark[][] result2 = new TicTacToe.Mark[3][3];
-        result2[0][0] = TicTacToe.Mark.X;
-        assertArrayEquals(result2, testField2.getField());
+        result2[0][0] = TicTacToe.Mark.O;
+        assertEquals(result2[0][0], testField2.getPoint(0,0));
 
         //тест на заполненность клетки
         TicTacToe testField3 = new TicTacToe(3);
@@ -28,10 +28,10 @@ class TicTacToeTest {
         testField3.addMark(0,0, TicTacToe.Mark.X);
         TicTacToe.Mark[][] result3 = new TicTacToe.Mark[3][3];
         result3[0][0] = TicTacToe.Mark.X;
-        assertArrayEquals(result3, testField3.getField());
+        assertEquals(result3[0][0], testField3.getPoint(0,0));
 
         TicTacToe testField4 = new TicTacToe(3);
-        assertThrows(ArrayIndexOutOfBoundsException.class,() -> testField1.addMark(4,4, TicTacToe.Mark.X));
+        assertThrows(IndexOutOfBoundsException.class,() -> testField1.addMark(4,4, TicTacToe.Mark.X));
 
     }
 
@@ -41,7 +41,7 @@ class TicTacToeTest {
         testField1.addMark(0,0, TicTacToe.Mark.X);
         testField1.removeMark(0,0);
         TicTacToe.Mark[][] result1 = new TicTacToe.Mark[3][3];
-        assertArrayEquals(result1, testField1.getField());
+        assertEquals(result1[0][0], testField1.getPoint(0,0));
 
     }
 
@@ -54,7 +54,7 @@ class TicTacToeTest {
         testField.addMark(0,2, TicTacToe.Mark.X);
         testField.addMark(2,0, TicTacToe.Mark.X);
         testField.addMark(2,1, TicTacToe.Mark.X);
-        assertEquals(3, testField.getLongest());
+        assertEquals(3, testField.getLongest(TicTacToe.Mark.X));
 
         //testMain
         TicTacToe testField1 = new TicTacToe(3);
@@ -63,7 +63,7 @@ class TicTacToeTest {
         testField1.addMark(1,0, TicTacToe.Mark.X);
         testField1.addMark(1,1, TicTacToe.Mark.X);
         testField1.addMark(2,2, TicTacToe.Mark.X);
-        assertEquals(3, testField1.getLongest());
+        assertEquals(3, testField1.getLongest(TicTacToe.Mark.X));
 
         //testSecondary
         TicTacToe testField2 = new TicTacToe(3);
@@ -72,7 +72,7 @@ class TicTacToeTest {
         testField2.addMark(1,1, TicTacToe.Mark.X);
         testField2.addMark(1,2, TicTacToe.Mark.X);
         testField2.addMark(2,0, TicTacToe.Mark.X);
-        assertEquals(3, testField1.getLongest());
+        assertEquals(3, testField1.getLongest(TicTacToe.Mark.X));
 
 
         //testVer
@@ -82,7 +82,7 @@ class TicTacToeTest {
         testField3.addMark(2,0, TicTacToe.Mark.X);
         testField3.addMark(0,1, TicTacToe.Mark.X);
         testField3.addMark(1,1, TicTacToe.Mark.X);
-        assertEquals(3, testField1.getLongest());
+        assertEquals(3, testField1.getLongest(TicTacToe.Mark.X));
 
 
         TicTacToe testField4 = new TicTacToe(5);
@@ -96,7 +96,7 @@ class TicTacToeTest {
         testField4.addMark(3,3, TicTacToe.Mark.X);
         testField4.addMark(3,2, TicTacToe.Mark.X);
         testField4.addMark(4,2, TicTacToe.Mark.X);
-        assertEquals(5, testField4.getLongest());
+        assertEquals(5, testField4.getLongest(TicTacToe.Mark.X));
     }
 
 }
